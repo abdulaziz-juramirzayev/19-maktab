7ae96025df9d517d93564f8bda2e1e6cc9dd47ab
abdulaziz-juramirzayev <templeuzb@gmail.com>
2022-04-06 17:51:03 +0500

    Maktabtaaaaaaaaa

---
+++ java/JavaScript.js
@@ -0,0 +1,63 @@
+
+//  Scroolbar
+
+let progress = document.getElementById('progressbar');
+let totalHeight = document.body.scrollHeight - 
+    window.innerHeight;
+window.onscroll = function() {
+    let progressHeight = (window.pageYOffset / 
+    totalHeight) * 100;
+    progress.style.height = progressHeight + "%";
+}
+
+// Counter number 
+
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach((counter) => {
+    counter.innerText = '0';
+    
+    const updateCounter = () => {
+        const target = +counter.getAttribute('data-target');
+        const c = + counter.innerText;
+
+        const increment = target / 250;
+
+        if (c < target) {
+            counter.innerText = `${Math.ceil(c + increment)}`
+            setTimeout(updateCounter, 50);
+        }
+        else {
+            counter.innerText = target;
+        }
+    };
+    updateCounter();
+});
+
+// Scroll Reveal
+
+ScrollReveal ({
+    reset: false,
+    distance: '80px',
+    duration: 1000
+});
+
+ScrollReveal().reveal('.explore',{delay: 200, origin: 'bottom'});
+ScrollReveal().reveal('.carddiv1',{delay: 200, origin: 'bottom'});
+ScrollReveal().reveal('.carddiv2',{delay: 300, origin: 'bottom'});
+ScrollReveal().reveal('.carddiv3',{delay: 400, origin: 'bottom'});
+ScrollReveal().reveal('.carddiv4',{delay: 500, origin: 'bottom'});
+ScrollReveal().reveal('.top-destination',{delay: 200, origin: 'bottom'});
+ScrollReveal().reveal('.bottom-destination',{delay: 600, origin: 'bottom'});
+ScrollReveal().reveal('#bottom-packages',{delay: 600, origin: 'bottom'});
+ScrollReveal().reveal('.counter-div',{delay: 200, origin: 'bottom'});
+ScrollReveal().reveal('.left-many',{delay: 300, origin: 'bottom'});
+ScrollReveal().reveal('.right-corusel',{delay: 400, origin: 'bottom'});
+ScrollReveal().reveal('.articles-flex',{delay: 200, origin: 'bottom'});
+ScrollReveal().reveal('.newsletter-div',{delay: 200, origin: 'bottom'});
+ScrollReveal().reveal('.flex-footer',{delay: 200, origin: 'bottom'});
+ScrollReveal().reveal('.about_title',{delay: 200, origin: 'bottom'});
+ScrollReveal().reveal('.right_customer',{delay: 200, origin: 'bottom'});
+ScrollReveal().reveal('.left_customer',{delay: 100, origin: 'bottom'});
+ScrollReveal().reveal('.accordion1',{delay: 200, origin: 'bottom'});
+ScrollReveal().reveal('.accordion2',{delay: 100, origin: 'bottom'});
